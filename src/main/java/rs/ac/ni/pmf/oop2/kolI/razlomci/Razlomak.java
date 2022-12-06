7ab7541f34b7d33c62a20ff54838cc2f9e8dfeb6
Marko Milosevic <ninja643@gmail.com>
2022-12-06 10:19:26 +0100

    equals & hashcode

--- src/main/java/rs/ac/ni/pmf/oop2/kolI/razlomci/Razlomak.java
+++ src/main/java/rs/ac/ni/pmf/oop2/kolI/razlomci/Razlomak.java
@@ -1,8 +1,10 @@
 package rs.ac.ni.pmf.oop2.kolI.razlomci;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class Razlomak
 {
     private final int brojilac;
@@ -70,6 +72,11 @@ public class Razlomak
         return new Razlomak(brojilac, imenilac);
     }
 
+    public static Razlomak nula()
+    {
+        return new Razlomak(0, 1);
+    }
+
     public Razlomak reciprocnaVrednost()
     {
         if (brojilac == 0)
@@ -79,4 +86,33 @@ public class Razlomak
 
         return new Razlomak(this.imenilac, this.brojilac);
     }
+
+//    @Override
+//    public boolean equals(Object obj)
+//    {
+//        System.out.println("equals() je pozvan");
+//        if (obj == null)
+//        {
+//            return false;
+//        }
+//
+//        if(!(obj instanceof Razlomak))
+//        {
+//            return false;
+//        }
+////        if(!(obj.getClass().equals(Razlomak.class)))
+////        {
+////            return false;
+////        }
+//
+//        final Razlomak drugi = (Razlomak) obj;
+//        return brojilac == drugi.brojilac && imenilac == drugi.imenilac;
+//    }
+
+//    @Override
+//    public int hashCode()
+//    {
+//        System.out.println("hashcode() je pozvan");
+//        return brojilac + imenilac;
+//    }
 }
--- src/test/java/rs/ac/ni/pmf/oop2/kolI/razlomci/RazlomakTest.java
+++ src/test/java/rs/ac/ni/pmf/oop2/kolI/razlomci/RazlomakTest.java
@@ -2,6 +2,11 @@ package rs.ac.ni.pmf.oop2.kolI.razlomci;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RazlomakTest
@@ -20,6 +25,8 @@ class RazlomakTest
         assertThat(new Razlomak(2, -8).toString()).isEqualTo("-1/4");
         assertThat(new Razlomak(-2, 8).toString()).isEqualTo("-1/4");
         assertThat(new Razlomak(12, 18).toString()).isEqualTo("2/3");
+
+        assertThat(Razlomak.nula().toString()).isEqualTo("0");
     }
 
     @Test
@@ -43,5 +50,78 @@ class RazlomakTest
         assertThat(Razlomak.of(3, 4).reciprocnaVrednost().toString()).isEqualTo("4/3");
         assertThat(Razlomak.of(-3, 4).reciprocnaVrednost().toString()).isEqualTo("-4/3");
         assertThat(Razlomak.of(1, 2).reciprocnaVrednost().toString()).isEqualTo("2");
+    }
+
+    @Test
+    public void proveraJednakosti1()
+    {
+        Razlomak razlomak1 = Razlomak.of(1, 4);
+        Razlomak razlomak2 = Razlomak.of(2, 8);
+
+        List<Razlomak> razlomci = new ArrayList<>();
+//        razlomci.add(razlomak1);
+        razlomci.add(razlomak2);
+
+        if (razlomci.contains(razlomak1))
+        {
+            System.out.println("Pronadjen!");
+        }
+
+        Set<Razlomak> skupRazlomaka = new HashSet<>();
+        skupRazlomaka.add(razlomak1);
+        skupRazlomaka.add(razlomak2);
+
+        System.out.println("Broj elemenata skupa: " + skupRazlomaka.size());
+
+        List<Integer> listaBrojeva = new ArrayList<>();
+        listaBrojeva.add(new Integer(5));
+        Integer broj = new Integer(5);
+        if (listaBrojeva.contains(broj))
+        {
+            System.out.println("Broj je pronadjen u listi");
+        }
+
+        Set<Integer> skupBrojeva = new HashSet<>();
+
+        skupBrojeva.add(new Integer(5));
+        skupBrojeva.add(new Integer(5));
+
+        System.out.println("Broj elemenata skupa: " + skupBrojeva.size());
+    }
+
+    @Test
+    public void proveraJednakosti2()
+    {
+        Razlomak razlomak1 = Razlomak.of(1, 2);
+        Razlomak razlomak2 = Razlomak.of(2, 3);
+        Razlomak razlomak3 = Razlomak.of(2, 4);
+
+        assertThat(razlomak1).isNotEqualTo(razlomak2);
+        assertThat(razlomak1).isNotEqualTo(5);
+        assertThat(razlomak1).isEqualTo(razlomak3);
+
+    }
+
+    @Test
+    public void proveraJednakosti3()
+    {
+        Set<Razlomak> razlomci = new HashSet<>();
+        System.out.println("Dodajem 10/3 u skup");
+        razlomci.add(Razlomak.of(10, 3));
+        System.out.println("Dodajem 4/9 u skup");
+        razlomci.add(Razlomak.of(4, 9));
+//        razlomci.add(Razlomak.of(3, 4));
+//        razlomci.add(Razlomak.of(4, 5));
+
+        System.out.println("Provera");
+
+        if (razlomci.contains(Razlomak.of(8, 18)))
+        {
+            System.out.println("Razlomak pronadjen");
+        }
+        else
+        {
+            System.out.println("Razlomak nije pronadjen");
+        }
     }
 }
